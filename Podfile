ecee2e6a8474264a540dbb05ce930a37cacc2722
ksemianov <semyanovk@gmail.com>
2021-09-26 16:15:19 +0300

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '11.0'
+
+use_frameworks!
+
+target 'RxGRDB.CompilationError' do
+  pod 'RxGRDB'
+end
---
+++ Pods/Target Support Files/Pods-RxGRDB.CompilationError/Pods-RxGRDB.CompilationError-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxGRDB_CompilationError : NSObject
+@end
+@implementation PodsDummy_Pods_RxGRDB_CompilationError
+@end
